cf6f769f3978410fc8d2cdc19153301d75b1753d
Joao V. F. Lima <jvlima@inf.ufsm.br>
2015-07-17 14:34:14 -0300

    Fix.

--- main.cpp
+++ main.cpp
@@ -41,10 +41,12 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
+#if 0
   if (argc!=2) {
     cerr << "Usage: sudo smartpower logfile" << endl;
     exit(-1);
   }
+#endif
 
 //  ofstream file;
 //  file.open(argv[1]);
